f123a40a5db8c79523e5fe950bd02437803bde51
TaherMustansir1929 <eren.jeager1929101@gmail.com>
2025-06-23 16:39:07 +0500

    09: Agents Setup

--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,15 +1,10 @@
 "use client";
 
-import { useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
-
 export const HomeView = () => {
-  const trpc = useTRPC();
-  const { data } = useQuery(trpc.hello.queryOptions({ text: "hello, world!" }))
 
   return (
-    <div className="p-4 flex flex-col gap-y-4">
-      {data?.greeting}
+    <div>
+      Home view
     </div>
   );
 };
