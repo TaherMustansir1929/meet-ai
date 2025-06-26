0fa172b71ca0d637d8a6580472477564d2c1f20c
TaherMustansir1929 <eren.jeager1929101@gmail.com>
2025-06-26 15:54:13 +0500

    20: Meeting Page

--- src/app/(dashboard)/meetings/[meetingId]/page.tsx
+++ src/app/(dashboard)/meetings/[meetingId]/page.tsx
@@ -1,8 +1,49 @@
-const MeetingIdPage = () => {
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
+import { auth } from "@/lib/auth";
+import { getQueryClient, trpc } from "@/trpc/server";
+
+import { 
+  MeetingIdView,
+  MeetingIdViewError,
+  MeetingIdViewLoading,
+} from "@/modules/meetings/ui/views/meeting-id-view";
+
+interface Props {
+  params: Promise<{
+    meetingId: string;
+  }>
+}
+
+const MeetingIdPage = async ({ params }: Props) => {
+  const { meetingId } = await params;
+  
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    redirect("/sign-in");
+  };
+  
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(
+    trpc.meetings.getOne.queryOptions({ id: meetingId }),
+  );
+  // TODO: prefetch meetings.getTranscript
+  
   return (
-    <div>
-      Meeting Id Page
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<MeetingIdViewLoading />}>
+        <ErrorBoundary fallback={<MeetingIdViewError />}>
+          <MeetingIdView meetingId={meetingId} />
+        </ErrorBoundary>
+      </Suspense>
+    </HydrationBoundary>
   )
 };
 
--- src/modules/agents/ui/views/agent-id-view.tsx
+++ src/modules/agents/ui/views/agent-id-view.tsx
@@ -12,7 +12,7 @@ import { Badge } from "@/components/ui/badge";
 
 import { useTRPC } from "@/trpc/client";
 import { toast } from "sonner";
-import { useConfirm } from "../../hooks/use-confirm";
+import { useConfirm } from "@/hooks/use-confirm";
 import { useState } from "react";
 import { UpdateAgentDialog } from "../components/update-agent-dialog";
 
